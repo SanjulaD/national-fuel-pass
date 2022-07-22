12d85ef84e25923d35dd5e78e37a48653f796365
SanjulaD <desanjula99@gmail.com>
2022-07-22 13:17:05 +0530

    feat: hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      yellow: {
+        100: '#FFF4DC',
+      },
+      red: {
+        100: '#8D153A',
+      },
+      orange: {
+        100: '#EB7400',
+        200: '#F19B47',
+      },
+    },
   },
   plugins: [],
 };
